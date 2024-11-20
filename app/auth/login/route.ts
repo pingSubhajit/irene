72c4b0c1641632fc2b7897f3b003a7f29224cfd6
Subhajit <subha60kundu@gmail.com>
2024-11-20 10:55:57 +0530

    attempt: Log default URL before login

--- app/auth/login/route.ts
+++ app/auth/login/route.ts
@@ -3,6 +3,7 @@ import {redirect} from 'next/navigation'
 import {defaultUrl} from '@/lib/constants'
 
 export const GET = async () => {
+	console.log(`${defaultUrl}/auth/callback`)
 	const supabase = await createClient()
 	const {data, error} = await supabase.auth.signInWithOAuth({
 		provider: 'google',
